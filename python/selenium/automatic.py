60e11399dbeb7c2a65d1824e90eeda2b922288d3
makeloff1 <makeloff1@gmail.com>
2018-06-04 23:37:21 +0900

    add test_file

---
+++ python/selenium/automatic.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as Ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+class
